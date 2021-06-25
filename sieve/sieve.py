6f935fdf90757a352a5b228f9b5ca9b8c97d6007
Alex Carter <60497471+acarter881@users.noreply.github.com>
2021-06-24 23:00:22 -0500

    Add files via upload

---
+++ sieve/sieve.py
@@ -0,0 +1,10 @@
+def primes(limit):
+    composites = []
+
+    for i in range(2, limit + 1):
+        if i not in composites:
+            tempList = [i for i in range(i, limit + 1, i) if i not in composites]
+            composites.extend(tempList[1:])
+            tempList = []
+    
+    return [num for num in range(2, limit + 1) if num not in composites]
