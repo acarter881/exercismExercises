d8aeda38ba46ef223579158a966fd9efdcd52626
Alex Carter <60497471+acarter881@users.noreply.github.com>
2021-06-27 18:43:48 -0500

    Add files via upload

---
+++ resistor-color/resistor_color.py
@@ -0,0 +1,5 @@
+def color_code(color):
+    return dict(zip(['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white'], range(10)))[color]
+
+def colors():
+    return ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
