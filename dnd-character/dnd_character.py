e6c13e35b5ba8b09246de880631a28c8d4580549
Alex Carter <60497471+acarter881@users.noreply.github.com>
2021-06-28 23:26:46 -0500

    Update dnd_character.py

--- dnd-character/dnd_character.py
+++ dnd-character/dnd_character.py
@@ -2,18 +2,12 @@ import random
 
 class Character:
     def __init__(self):
-        self.strength = sum(sorted([random.randint(1,6) for i in range(4)])[1:])
-        self.dexterity = sum(sorted([random.randint(1,6) for i in range(4)])[1:])
-        self.constitution = sum(sorted([random.randint(1,6) for i in range(4)])[1:])
-        self.intelligence = sum(sorted([random.randint(1,6) for i in range(4)])[1:])
-        self.wisdom = sum(sorted([random.randint(1,6) for i in range(4)])[1:])
-        self.charisma = sum(sorted([random.randint(1,6) for i in range(4)])[1:])
+        self.strength = self.dexterity = self.constitution = self.intelligence = self.wisdom = self.charisma = sum(sorted([random.randint(1,6) for i in range(4)])[1:])
         self.hitpoints = 10 + modifier(self.constitution)
         
     def ability(self):
         self.ability = random.choices([self.strength, self.dexterity, self.constitution, self.intelligence, self.wisdom, self.charisma])[0]
         return self.ability
-
 
 def modifier(num):
     return (num - 10) // 2
